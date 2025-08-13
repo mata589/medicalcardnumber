34bc3f14ea064dc156aea1f4484bd38f45bc392b
Richard Kizito <rkizito@ICEA_UG2.COM>
2025-08-13 15:32:49 +0300

    Updates

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import sql from "mssql";
 const config = {
   user: "medicalapp_user",             // database user
   password: "medicalapp_user",         // password for that user
-  server: "10.9.0.130",                // SQL Server IP
+  server: "ICEA-TEST-001\NAVDEMO",                // SQL Server IP
   database: "MedicalAppDB",            // your database name
   port: 1433,                           // default SQL Server port
   options: {
